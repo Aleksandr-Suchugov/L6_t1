625c71ee8b4228d09cf07552d1c7245464179af3
Aleksandr Suchugov <alex.coup@yandex.ru>
2022-03-19 18:04:35 +0300

    test added

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ export default function orderByProps(obj, [key1, key2]) {
   for (const prop in obj) {
     if (obj.hasOwnProperty(prop)) {
       if ((prop === key1) || (prop === key2)) {
-        outputArr.unshift({ key: prop, value: obj[prop] });
+        outputArr.push({ key: prop, value: obj[prop] });
         delete obj[prop];
       }
     }
   }
-  const otherPropArr = obj.entries.sort((a, b) => a[0] - b[0]);
+  const otherPropArr = Object.entries(obj).sort();
   otherPropArr.forEach((el) => outputArr.push({ key: el[0], value: el[1] }));
   return outputArr;
 }
